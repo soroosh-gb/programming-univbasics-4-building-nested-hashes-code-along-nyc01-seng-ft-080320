baca980a0a586e007ab57884155058b79543690f
Soroosh Golbabae <soroosh.gb@gmail.com>
2020-06-29 21:35:51 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -13,7 +13,7 @@ def fourth_challenge
         {
           :name => "Mercutio",
           :age => "18",
-          :attitude => "hot_headed"
+          :attitude => "hot-headed"
         }
       ]
    }, 
