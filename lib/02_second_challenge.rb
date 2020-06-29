386e0c6a615eb6272204cae30d45c2b49729d719
Soroosh Golbabae <soroosh.gb@gmail.com>
2020-06-29 21:08:41 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,7 +1,10 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {},
+      :patriarch => {
+        :name => "Lord Montague",
+        :age => 
+      },
       :matriarch => {},
       :hero => {},
       :hero_friends => []
