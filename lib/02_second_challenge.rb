78decb3c19345dba90a0e4677fef8ac4aaa313c2
Soroosh Golbabae <soroosh.gb@gmail.com>
2020-06-29 21:32:13 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,58 +1,16 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {
-        :name => "Lord Montague",
-        :age => "53"
-      },
-      :matriarch => {
-        :name => "lady Montague",
-        :age => "54"
-      },
-      :hero => {
-        :name => "Romeo",
-        :age => "15",
-        :status => "alive"
-      },
-      :hero_friends => [
-        {
-          :name => "Benvolio",
-          :age => "17",
-          :attitude => "worried"
-        },
-        {
-          :name => "Mercutio",
-          :age => "18",
-          :attitude => "hot_headed"
-        }
-      ]
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
     }, 
     :capulet => {
-      :patriarch => {
-        :name => "Lord capulet",
-        :age => "50"
-      },
-      :matriarch => {
-        :name => "Lady Capulet",
-        :age => "51"
-      },
-      :heroine => {
-        :name => "Juliet",
-        :age => "15",
-        :status => "alive"
-      },
-      :heroine_friends => [
-        {
-          :name => "Steven",
-          :age => "30",
-          :attitude => "confused"
-        },
-        {
-          :name => "Nurse",
-          :age => "44",
-          :attitude => "worried"
-        }
-      ]
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
       
      }
   }
